a617150fb39adc77258a5f78072a6c5b6fa38390
Rottecraft <zbc24sdav@zbc.dk>
2025-08-25 11:11:12 +0200

    startup

---
+++ f1case1/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+  return(
+  <> 
+    <Header/>
+    <Body/>
+    <Footer/>
+  </>
+//to run this app, type 'cd f1case1', then 'npm run dev'
+  );
+ 
+ 
+}
+
+export default App
---
+++ f1case1/src/Components/BasicLayout/BasicLayout.jsx
@@ -0,0 +1,3 @@
+import styles from './BasicLayout.module.css'
+
+className={styles.videoContainer} 
